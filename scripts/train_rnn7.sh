d535ad3c903d1a8dab3e9b00962bde1d7a268062
lindenmp <lindenparkes@gmail.com>
2023-12-02 11:03:19 -0500

    new run

--- scripts/train_rnn7.sh
+++ scripts/train_rnn7.sh
@@ -6,21 +6,23 @@ seq_len_base=13
 standardize_task='True'
 kernel_type='sa_axis'
 
+seq_len_multi=5
 rnn_model='rnn-tanh'
 hidden_size=100
-n_runs=50
+batch_size=32
+n_runs=25
 n_epochs=30000
 reg_type='l2'
 
 for mask_weights in 'True' 'False'; do
-  for reg_weight in 0.0 0.0001 0.001 0.002 0.003; do
-    for decision in 400 300 200; do
+  for reg_weight in 0.0001 0.001 0.0015 0.002 0.003; do
+    for decision in 400 300 200 100; do
       seq_len=$(( seq_len_base + ((decision-100)/100) ))
+      seq_len=$(( seq_len * seq_len_multi ))
 
-      python ${scriptsdir}/train_rnn.py --task ${task} --rnn_model ${rnn_model} --kernel_type ${kernel_type} \
-      --mask_weights ${mask_weights} --hidden_size ${hidden_size} --n_runs ${n_runs} --n_epochs ${n_epochs} \
-      --decision ${decision} --seq_len ${seq_len} --outdir ${outdir} --reg_weight ${reg_weight} --reg_type ${reg_type} \
-      --standardize_task ${standardize_task}
+      python ${scriptsdir}/train_rnn.py --outdir ${outdir} --task ${task} --seq_len ${seq_len} --standardize_task ${standardize_task} --kernel_type ${kernel_type} \
+      --rnn_model ${rnn_model} --hidden_size ${hidden_size} --batch_size ${batch_size} --n_runs ${n_runs} --n_epochs ${n_epochs} --reg_type ${reg_type} \
+      --mask_weights ${mask_weights} --reg_weight ${reg_weight} --decision ${decision}
     done
   done
 done
