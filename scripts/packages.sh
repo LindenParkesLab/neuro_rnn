7c141125f66ea608f0773c06e2f265c45b869771
Linden Parkes <lindenparkes@gmail.com>
2023-12-10 10:45:58 -0500

    updated to install environment on macbook

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,8 +1,5 @@
-# Install packages
-pip install --upgrade pip
-pip install --upgrade setuptools
+# install packages: linux
 
-# utils
 conda install ipython tqdm numpy scipy pandas scikit-learn scikit-learn-intelex matplotlib seaborn jupyterlab \
 ipywidgets networkx numba pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 pip install gym nctpy
@@ -23,4 +20,25 @@ pip install .
 cd ~/research_projects/neuro_rnn
 conda env export > environment.yml --no-builds
 
+python setup.py install
+
+# install packages: macbook
+project_dir='/Users/lindenmp/Google-Drive-Rutgers/work/research_projects/neuro_rnn'
+pip install --upgrade pip
+pip install --upgrade setuptools
+
+conda install ipython tqdm numpy scipy pandas scikit-learn matplotlib seaborn jupyterlab \
+ipywidgets networkx numba pytorch::pytorch torchvision torchaudio -c pytorch
+pip install --upgrade notebook
+pip install gym nctpy
+
+git clone https://github.com/gyyang/neurogym.git ${project_dir}/packages/neurogym
+cd ${project_dir}/packages/neurogym
+pip install -e .
+
+git clone https://github.com/aestrivex/bctpy.git ${project_dir}/packages/bctpy
+cd ${project_dir}/packages/bctpy
+pip install .
+
+cd ${project_dir}
 python setup.py install
